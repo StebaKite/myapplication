6f6591dfa7410cffc2c17cd808b227c9714ca9b3
Barbieri Stefano <s.barbieri@nexigroup.com>
2023-12-15 14:43:25 +0100

    preparazione secondo fragment con navigazione

--- app/src/main/java/com/example/myapplication/FirstFragment.java
+++ app/src/main/java/com/example/myapplication/FirstFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -15,46 +16,66 @@ import com.example.myapplication.databinding.FragmentFirstBinding;
 public class FirstFragment extends Fragment {
 
     private FragmentFirstBinding binding;
+    TextView showCountTextView;
 
-    @Override
-    public View onCreateView(
-            LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState
-    ) {
-
-        binding = FragmentFirstBinding.inflate(inflater, container, false);
-        return binding.getRoot();
-
-    }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        binding.randomButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                NavHostFragment.findNavController(FirstFragment.this)
-                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
-            }
-        });
-
         view.findViewById(R.id.random_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                NavHostFragment.findNavController(FirstFragment.this)
-                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
+                int currentCount = Integer.parseInt(showCountTextView.getText().toString());
+                FirstFragmentDirections.ActionFirstFragmentToSecondFragment action = FirstFragmentDirections.actionFirstFragmentToSecondFragment(currentCount);
+                NavHostFragment.findNavController(FirstFragment.this).navigate(action);
             }
         });
 
         view.findViewById(R.id.toast_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast myToast = Toast.makeText(getActivity(), "Hello toast!", Toast.LENGTH_SHORT);
+                Toast myToast = Toast.makeText(getActivity(), R.string.toast_message, Toast.LENGTH_SHORT);
                 myToast.show();
             }
         });
 
+        view.findViewById(R.id.count_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                countMe(view);
+            }
+        });
 
+    }
+
+    /*
+    Potresti chiamare findViewById() in countMe() per trovare showCountTextView.
+    Tuttavia, countMe() viene chiamato ogni volta che si fa clic sul pulsante e findViewById()
+    è un metodo relativamente dispendioso in termini di tempo da chiamare.
+    Quindi è meglio trovare la view la prima volta e memorizzarla nella cache.
+
+    Quindi: nn onCreateView(), chiamerai findViewById() per ottenere TextView che mostra il conteggio.
+    Il metodo findViewById() deve essere chiamato su una vista in cui dovrebbe iniziare la ricerca dell'ID richiesto,
+    quindi assegna la vista di layout attualmente restituita a una nuova variabile, fragmentFirstLayout.
+     */
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View fragmentFirstLayout = inflater.inflate(R.layout.fragment_first, container, false);
+        showCountTextView = fragmentFirstLayout.findViewById(R.id.textview_first);
+        return fragmentFirstLayout;
+    }
+
+    public void countMe(View view) {
+        // Prendo il valore dell'oggetto
+        String countString = showCountTextView.getText().toString();
+
+        // converto il valore prelevato dall'oggetto in Intero e incremento il valore di uno
+        Integer count = Integer.parseInt(countString);
+        count++;
+
+        // imposto il valore dell'oggetto con il nuovo valore incrementato
+        showCountTextView.setText(count.toString());
     }
 
     @Override
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,10 @@
+buildscript {
+    val navigationSafeArgsGradlePluginVersion1 by extra("2.7.6")
+}
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "8.1.3" apply false
 }
+val navigationSafeArgsGradlePluginVersion by extra("2.7.6")
+val navigationSafeArgsGradlePluginVersion1 by extra(navigationSafeArgsGradlePluginVersion)
+val navigationSafeArgsGradlePluginVersion2 by extra(navigationSafeArgsGradlePluginVersion)
